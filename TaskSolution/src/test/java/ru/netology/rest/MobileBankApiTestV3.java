68357b252d525908104dc00fd2a5fdd27297559f
Texnolog24 <latkina0808@yandex.ru>
2025-05-10 17:28:50 +0500

    Внесли изменения в тест

--- TaskSolution/src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ TaskSolution/src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -18,7 +18,7 @@ class MobileBankApiTestV3 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200)
+                .statusCode(204)
                 // специализированные проверки - лучше
                 .contentType(ContentType.JSON)
                 .body("", hasSize(3))
